9548d98e4a6d7ecb2ecd021b0c6ae8a22b25ab40
sergey <szaprudnov@hotmail.com>
2023-08-27 21:24:57 +0300

    add classes

--- habbit/views.py
+++ habbit/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+
+class HabbitCreateAPIView(CreateAPIView):
+    pass
+
+
+class HabbitListAPIView(ListAPIView):
+    pass
+
+
+class HabbitRetrieveAPIView(RetrieveAPIView):
+    pass
+
+
+class HabbitUpdateAPIView(UpdateAPIView):
+    pass
+
+
+class HabbitDestroyAPIView(DestroyAPIView):
+    pass
