e0223a7b6b53c01fbf32379462a89530f200c9c4
sergey <szaprudnov@hotmail.com>
2023-08-27 22:36:24 +0300

    add description serialisers/habbit

---
+++ habbit/serialisers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from habbit.models import Habbit
+from habbit.validators import validate_time_complete, validate_related_habbit, validate_period
+from users.models import User
+
+
+class HabbitSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+    time_complete = serializers.IntegerField(validators=[validate_time_complete])
+    related_habbit = serializers.PrimaryKeyRelatedField(queryset=Habbit.objects.all(), required=False, validators=[validate_related_habbit])
+    period = serializers.IntegerField(validators=[validate_period])
+
+    class Meta:
+        model = Habbit
+        fields = '__all__'
