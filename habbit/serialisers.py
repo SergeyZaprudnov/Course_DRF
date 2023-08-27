f01a38d191e2b829ea2cdfc90b11f7b12cd7ea62
sergey <szaprudnov@hotmail.com>
2023-08-27 22:37:18 +0300

    PEP8

--- habbit/serialisers.py
+++ habbit/serialisers.py
@@ -8,7 +8,8 @@ from users.models import User
 class HabbitSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
     time_complete = serializers.IntegerField(validators=[validate_time_complete])
-    related_habbit = serializers.PrimaryKeyRelatedField(queryset=Habbit.objects.all(), required=False, validators=[validate_related_habbit])
+    related_habbit = serializers.PrimaryKeyRelatedField(queryset=Habbit.objects.all(), required=False,
+                                                        validators=[validate_related_habbit])
     period = serializers.IntegerField(validators=[validate_period])
 
     class Meta:
