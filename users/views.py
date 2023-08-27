e97aa68f1386e5f7a2916f4b77f85accaf905c86
sergey <szaprudnov@hotmail.com>
2023-08-27 21:39:31 +0300

    add classes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+
+class UserCreateAPIView(CreateAPIView):
+    pass
+
+
+class UserListAPIView(ListAPIView):
+    pass
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    pass
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    pass
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    pass
