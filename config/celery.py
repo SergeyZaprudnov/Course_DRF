19099e87d34c628e3204115821f9c9b4906a5c67
sergey <szaprudnov@hotmail.com>
2023-08-28 23:53:41 +0300

    editing /config

--- config/celery.py
+++ config/celery.py
@@ -5,5 +5,5 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTING_MODULE', 'config.settings')
 
 app = Celery('config')
-app.config_from_object('django.conf:settings', namespase='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
