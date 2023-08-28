567d717b0f1f468cce9371b7078292a16ad2c261
sergey <szaprudnov@hotmail.com>
2023-08-28 22:34:14 +0300

    adding descriptions /habbit

--- habbit/views.py
+++ habbit/views.py
@@ -1,22 +1,52 @@
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from habbit.models import Habbit
+from habbit.pagination import MyPagination
+from habbit.permissions import IsPublicOrReadOnly, IsOwnerOrReadOnly
+from habbit.serialisers import HabbitSerializer
 
 
 class HabbitCreateAPIView(CreateAPIView):
-    pass
+    serializer_class = HabbitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
 
 
 class HabbitListAPIView(ListAPIView):
-    pass
+    serializer_class = HabbitSerializer
+    permission_classes = [IsAuthenticated, IsPublicOrReadOnly]
+    pagination_class = MyPagination
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Habbit.objects.filter(Q(public=True) | Q(user=user))
+        else:
+            return Habbit.objects.filter(public=True)
 
 
 class HabbitRetrieveAPIView(RetrieveAPIView):
-    pass
+    serializer_class = HabbitSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Habbit.objects.all()
 
 
 class HabbitUpdateAPIView(UpdateAPIView):
-    pass
+    serializer_class = HabbitSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Habbit.objects.all()
 
 
 class HabbitDestroyAPIView(DestroyAPIView):
-    pass
+    queryset = Habbit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
