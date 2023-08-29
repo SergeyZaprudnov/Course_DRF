a2fe31a8cc2a98bbd7fd6b9c484e65cdf04d26a1
sergey <szaprudnov@hotmail.com>
2023-08-29 19:23:26 +0300

    editing

--- habbit/tests.py
+++ habbit/tests.py
@@ -6,7 +6,9 @@ from users.models import User
 from django.urls import reverse
 
 
-class HabbitTestCase(APITestCase):
+# Create your tests here.
+
+class HabitTestCase(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create_user(
@@ -14,56 +16,58 @@ class HabbitTestCase(APITestCase):
             password='test'
         )
         self.client.force_authenticate(user=self.user)
+
         self.habbit = Habbit.objects.create(
             user=self.user,
             place='Квартира',
             time="10:00:00",
-            action='Йога',
-            useful=True,
+            action='Уборка',
+            pleasant=True,
             period=1,
-            time_complete=10
+            time_to_complete=10
         )
 
-    def test_create_habit(self):
-        url = reverse('habbit:habbit-create')
+    def test_create_habbit(self):
+        url = reverse('habbit:habbit_create')
         data = {
             'user': self.user.id,
-            'place': 'Стадион',
+            'place': 'Дом',
             'time': '08:00:00',
-            'action': 'Бег',
-            'useful': False,
+            'action': 'Игра в Lineage II',
+            'pleasant': False,
             'period': 8,
             'time_complete': 15
         }
 
         response = self.client.post(url, data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Habbit.objects.count(), 2)
 
-    def test_list_habits(self):
-        url = reverse('habbit:habbit-list')
+    def test_list_habbits(self):
+        url = reverse('habbit:habbit_list')
         self.client.force_authenticate(user=self.user)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 4)
 
     def test_retrieve_habit(self):
-        url = reverse('habbit:habbit-get', args=[self.habbit.id])
+        url = reverse('habbit:habbit_get', args=[self.habbit.id])
         self.client.force_authenticate(user=self.user)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['place'], 'Квартира')
 
     def test_update_habit(self):
-        url = reverse('habbit:habbit-update', args=[self.habbit.id])
+        url = reverse('habbit:habbit_update', args=[self.habbit.id])
         data = {'place': 'Офис'}
         self.client.force_authenticate(user=self.user)
         response = self.client.patch(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['place'], 'Офис')
 
-    def test_destroy_habbit(self):
-        url = reverse('habbit:habbit-delete', args=[self.habbit.id])
+    def test_destroy_habit(self):
+        url = reverse('habbit:habbit_delete', args=[self.habbit.id])
         self.client.force_authenticate(user=self.user)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
